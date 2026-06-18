2ca2fe5e4709aed0d03d0f7179b6260bfc00168f
sateesh16 <sateeshs1916@gmail.com>
2026-06-18 07:14:29 +0530

    Modified Route file

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ dotenv.config();
 import { httpServer } from './app';
 import connectDB from './config/database';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
-  await connectDB(); // connect DB first
+  await connectDB();
 
   httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
